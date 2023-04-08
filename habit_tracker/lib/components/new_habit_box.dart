91e5cc0b031631deeac0f8c54283e3c230dd3955
av1nDs0uza <avinalricdsouza@gmail.com>
2023-04-08 13:32:03 +0530

    habit tile ui

--- habit_tracker/lib/components/new_habit_box.dart
+++ habit_tracker/lib/components/new_habit_box.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class EnterNewHabitBox extends StatelessWidget {
-  const EnterNewHabitBox({super.key});
+  final controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+  const EnterNewHabitBox({super.key, required this.controller, required this.onSave, required this.onCancel});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: Colors.grey[700],
       content: TextField(
+        style: TextStyle(color: Color.fromRGBO(255,255,255,1)),
         decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
         ),
       ),
       actions: [
